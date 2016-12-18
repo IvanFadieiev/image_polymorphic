7d7a933d540d63e5103afd8fa01a378f2ecb2188
IvanFadieiev <ivanfadeev91@gmail.com>
2016-12-18 18:37:47 +0200

    image_upload_paperclip_google_drive

--- Gemfile
+++ Gemfile
@@ -50,4 +50,6 @@ gem 'bootstrap', '~> 4.0.0.alpha3'
 gem 'bootstrap-sass'
 gem 'bootstrap-generators'
 gem 'record_tag_helper', '~> 1.0'
-gem "paperclip", "~> 5.0.0"
+gem "paperclip", '~>4.2'
+gem 'paperclip-googledrive-new'
+gem 'google-api-client', '0.8.2', require: 'google/api_client'
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,19 @@
 class Image < ApplicationRecord
   belongs_to :imageable, polymorphic: true, optional: true
   has_attached_file :image,
-                     styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+    storage: :google_drive,
+    google_drive_credentials: "#{Rails.root}/config/google_drive.yml",
+    styles: { medium: "300x300", thumb: '100x100' },
+    google_drive_options: {
+      public_folder_id: '0B_O5YQUKw2SiOGFDMXo4VWo3OU0'
+    }
+  validates_attachment :image, content_type: {
+    content_type: [
+      "image/jpg",
+      "image/jpeg",
+      "image/png",
+      "image/gif"]
+    }
 
   def url(*args)
     image.url(*args)
